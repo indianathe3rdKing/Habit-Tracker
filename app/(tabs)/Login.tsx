e2469114b42b9d6e66bc59ee1e72d070ae8a8d40
indianathe3rd <indiemenziwa@gmail.com>
2025-10-26 13:51:53 +0200

    auth

---
+++ app/(tabs)/Login.tsx
@@ -0,0 +1,23 @@
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "bold",
+          height: 200,
+          width: 200,
+          backgroundColor: "coral",
+          color: "white",
+          borderRadius: 100,
+          textAlign: "center",
+          lineHeight: 200,
+        }}
+      >
+        Login Screen
+      </Text>
+    </View>
+  );
+}
